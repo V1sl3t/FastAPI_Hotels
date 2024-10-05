37545b631e82d0e8bfb2c939fd6ac7cc3051b6ae
Степан Суппес <suppes214@gmail.com>
2024-10-05 04:58:56 +0300

    added get bookings endpoints

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -6,11 +6,21 @@ from src.schemas.bookings import BookingAddRequest, BookingAdd
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
+@router.get("", summary="Получение комнат")
+async def get_all_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get("/me", summary="Получение комнаты")
+async def get_my_bookings(db: DBDep, user_id: UserIdDep):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
 @router.post("", summary="Создание брони")
-async def create_room(db: DBDep, user_id: UserIdDep, booking_data: BookingAddRequest):
-    room_price = await db.rooms.get_one_or_none(id=booking_data.model_dump()["room_id"])
-    new_room_data = BookingAdd(price=room_price.price, user_id=user_id, **booking_data.model_dump())
-    result = await db.bookings.add(new_room_data)
+async def create_booking(db: DBDep, user_id: UserIdDep, booking_data: BookingAddRequest):
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    new_booking_data = BookingAdd(price=room.price, user_id=user_id, **booking_data.model_dump())
+    result = await db.bookings.add(new_booking_data)
     await db.commit()
 
     return {"status": "OK", "data": result}
