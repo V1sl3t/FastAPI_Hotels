e747d626367e81d8ebd10caad2c69593bb0b6ec8
V1sl3t <suppes214@gmail.com>
2024-10-16 23:28:49 +0000

    Master

--- src/main.py
+++ src/main.py
@@ -40,4 +40,4 @@ app.include_router(router_comforts)
 app.include_router(router_images)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
